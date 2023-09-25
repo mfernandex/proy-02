f98bfd56585a5a5c49fdcf7cff80a42ad64a48cb
mfernandex <mfernandez@dim.uchile.cl>
2023-09-25 18:06:30 +0000

    mejora lectura del codigo

--- ex-01.py
+++ ex-01.py
@@ -7,7 +7,9 @@ group.add_argument("-v", "--verbose", action="store_true")
 group.add_argument("-q", "--quiet", action="store_true")
 parser.add_argument("x", type=int, help="base")
 parser.add_argument("y", type=int, help="exponente")
+
 args = parser.parse_args()
+
 answer = args.x**args.y
 
 if args.quiet:
